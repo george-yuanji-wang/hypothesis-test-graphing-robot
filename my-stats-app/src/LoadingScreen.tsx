bc2b52a07e7e1f3fa005a6a66ea9cdfcbecb31be
George Yuanji Wang <ddttwang@gmail.com>
2025-03-23 22:25:06 -0700

    screen

--- my-stats-app/src/LoadingScreen.tsx
+++ my-stats-app/src/LoadingScreen.tsx
@@ -5,6 +5,12 @@ export default function LoadingScreen() {
         Loading environment... be patient.
       </h1>
       <p className="mt-4 text-sm text-center">Created by George Wang</p>
+
+      <div>
+        <p className="mt-15 text-m text-center">Works best on a bigger screen—laptop or desktop recommended.</p>
+        <p>Report bugs: gwang10@opusd.us</p>
+      </div>
     </div>
+
   );
 }
